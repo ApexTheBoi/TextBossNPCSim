f894db634db9e5e2a5318064a714d4a8398a1656
Paul Coppolino <hta.infinite@gmail.com>
2025-02-23 00:02:37 -0500

    Initial Creation

---
+++ Boss.java
@@ -0,0 +1,12 @@
+public class Boss{
+    
+    private String name;
+    private int health;
+    private String arena;
+    
+    public Boss(String n, int h, String a){
+        name = n;
+        health = h;
+        arena = a;
+    }
+}
